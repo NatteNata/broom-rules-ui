3fd4e19f1e56d8b8c3696928fc06cd06ec334304
natte <natte.noshimo@gmail.com>
2024-10-27 23:08:17 +0300

    chore: tailwind works in  Storybook via new script which outputs build.css. Is there a better way?

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -18,14 +18,14 @@ export const Button = forwardRef<ElementRef<"button">, Props>(
                 className={cn('capitalize',
                     "flex items-center cursor-pointer box-border gap-2 p-1.5 px-6 rounded transition-colors ease-in-out",
                     variant === "primary" &&
-                    "bg-orange-400 hover:bg-accent-100 active:bg-accent-700 disabled:bg-accent-900 disabled:text-light-900",
+                    "bg-accent-500 hover:bg-accent-100 active:bg-accent-700 disabled:bg-accent-900 disabled:text-light-900",
                     variant === "secondary" &&
                     "bg-dark-300 hover:bg-dark-100 active:bg-dark-400 disabled:bg-dark-500 disabled:text-light-900",
                     variant === "outlined" &&
                     "border border-gray-300 text-accent-500 hover:text-accent-100 hover:border-accent-100 active:text-accent-700 active:border-accent-700 disabled:text-accent-900 disabled:border-accent-900",
                     variant === "ghost" &&
                     "text-accent-500 hover:text-accent-100 active:text-accent-700 disabled:text-accent-900",
-                    fullWidth && "w-full",
+                    fullWidth && "flex justify-center w-full",
                     className
                 )}
             />
