926141c850867210f9c4f8328721cb5c311d59c3
natte <natte.noshimo@gmail.com>
2024-10-29 02:54:47 +0300

    refactor: header component

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,4 +1,3 @@
-import {Logo} from "./Logo.tsx";
 import {cn} from "../../utils/merge-class-names.ts";
 
 type Props = {
@@ -10,7 +9,6 @@ export const Header = ({children, className}: Props) => {
     return (
         <header
             className={cn('flex bg-dark-700 border-b border-dark-300 items-center justify-between px-6 py-4', className)}>
-            <Logo/>
             {children}
         </header>
     )
