417f22f7e3ad1bb5597989c1651ba6c235831fbb
natte <natte.noshimo@gmail.com>
2024-11-02 18:20:49 +0300

    refactor: simple input

--- src/components/input/input.component.tsx
+++ src/components/input/input.component.tsx
@@ -1,21 +1,59 @@
-import { useGenerateId } from '../../utils/useGenerateId'
+import { ComponentPropsWithoutRef, ElementRef, ReactNode, forwardRef } from 'react'
+
+import { cn, useGenerateId } from '@/utils'
+import { SearchIcon } from 'lucide-react'
 
 type CommonProps = {
-  id?: string
-  label?: string
-  name?: string
-  type: string
-}
+  error?: ReactNode
+  label?: ReactNode
+} & ComponentPropsWithoutRef<'input'>
 
-const Input = ({ id, label, name, type }: CommonProps) => {
+const SimpleInput = forwardRef<HTMLInputElement, CommonProps>((props, ref) => {
+  const { className, error, id, label, name, placeholder, required, type = 'text' } = props
   const ID = useGenerateId(id)
 
   return (
     <div>
-      <label htmlFor={'ID'}>{label}</label>
-      <input id={ID} name={name} type={type}></input>
+      <label className={'block'} htmlFor={'ID'}>
+        <span
+          className={cn(
+            'text-light-900 text-sm/6 mb-1.5',
+            required && "after:content-['*'] after:ml-0.5 after:text-red-500"
+          )}
+        >
+          {label}
+        </span>
+        <input
+          className={cn(
+            'bg-inherit border border-dark-100 shadow-sm shadow-light-900 rounded-sm block',
+            'text-base/6 py-1.5 px-3',
+            'hover: text-light-900 border border-light-900 placeholder-light-900',
+            'focus-visible:border-none focus-visible:outline-none focus-visible:ring focus-visible:ring-accent-500',
+            'active: text-light-100 border border-light-100',
+            'invalid:border-red-500',
+            className
+          )}
+          id={ID}
+          name={name}
+          placeholder={placeholder}
+          ref={ref}
+          type={type}
+          {...props}
+        ></input>
+        {error ?? <span className={'text-red-500'}>{error}</span>}
+      </label>
+    </div>
+  )
+})
+
+export default SimpleInput
+
+const SearchInput = () => {
+  return (
+    <div>
+      <span className={'sr-only'}>Search</span>
+      <SearchIcon className={'absolute inset-y-12 left-4 flex items-center pl-3'} />
+      <SimpleInput name={''} />
     </div>
   )
 }
-
-export default Input
--- src/components/input/input.stories.tsx
+++ src/components/input/input.stories.tsx
@@ -1,16 +1,57 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import Input from './input.component'
+import SimpleInput from './input.component'
 
 const meta = {
-  component: Input,
-  title: 'Input Component',
-} satisfies Meta<typeof Input>
+  argTypes: {
+    type: {
+      control: 'select',
+      description: 'Select an input type',
+      options: ['email', 'search', 'text', 'tel', 'password'],
+    },
+  },
+  component: SimpleInput,
+  tags: ['autodocs'],
+  title: 'Components/Input',
+} satisfies Meta<typeof SimpleInput>
 
+export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default = {
+export const Default: Story = {
   args: {
     label: 'Email',
+    name: 'email',
+    placeholder: 'yourmail@example.com',
   },
+}
+
+export const Required: Story = {
+  args: {
+    label: 'Email',
+    name: 'email',
+    placeholder: 'yourmail@example.com',
+    required: true,
+  },
+  render: args => (
+    <div>
+      <SimpleInput {...args} />
+      <SimpleInput {...args} />
+    </div>
+  ),
+}
+
+export const Search: Story = {
+  args: {
+    name: 'search',
+    placeholder: 'yourmail@example.com',
+    required: true,
+    type: 'search',
+  },
+  render: args => (
+    <div>
+      <SimpleInput {...args} />
+      <SimpleInput {...args} />
+    </div>
+  ),
 }
