5101229cb8adeaa911fde42b84177c2fc30d08c6
NatteNata <natte@yandex.ru>
2025-04-24 10:06:12 +0300

    chore: input autofill and icons

--- src/assets/icons/components/Eye.tsx
+++ src/assets/icons/components/Eye.tsx
@@ -1,20 +1,20 @@
 import { type Ref, type SVGProps, forwardRef, memo } from 'react'
 const SvgEye = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
 	<svg
-		height={'24'}
+		xmlns='http://www.w3.org/2000/svg'
+		width='24'
+		height='24'
+		viewBox='0 0 24 24'
+		fill='none'
+		stroke='currentColor'
+		stroke-width='2'
+		stroke-linecap='round'
+		stroke-linejoin='round'
 		ref={ref}
-		viewBox={'0 0 24 24'}
-		width={'24'}
-		xmlns={'http://www.w3.org/2000/svg'}
 		{...props}
 	>
-		<path d={'M12 13.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3'} fill={'white'} />
-		<path
-			d={
-				'M21.87 11.5c-.64-1.11-4.16-6.68-10.14-6.5-5.53.14-8.73 5-9.6 6.5a1 1 0 0 0 0 1c.63 1.09 4 6.5 9.89 6.5h.25c5.53-.14 8.74-5 9.6-6.5a1 1 0 0 0 0-1m-9.87 4a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7'
-			}
-			fill={'black'}
-		/>
+		<path d='M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0' />
+		<circle cx='12' cy='12' r='3' />
 	</svg>
 )
 const ForwardRef = forwardRef(SvgEye)
